2bee10d96b5f6594e1596e1e5875948cf82e58c8
AudraK <AudraK@AUDRA-LAPTOP>
2025-01-17 16:08:36 -0700

    Add project files.

---
+++ DiceStimulator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DiceStimulator
+{
+    private Random randomnum;
+    public DiceStimulator()
+    {
+            randomnum = new Random();
+    }
+    public int[] RollDice(int numrolls)
+    {
+        //Create array to hold count of sums of dice
+        int[] rollCounts = new int[13];
+
+        // for # of times user entered
+        for (int i = 0; i < numrolls; i++)
+        {
+            // Roll die 1, collect result
+            int diceRoll = randomnum.Next(1, 7); // Generates a number from 1 to 6 (inclusive)
+
+            // roll die 2, collect result
+            int diceRoll2 = randomnum.Next(1, 7); // Generates a number from 1 to 6 (inclusive)
+
+            // Add together and store result
+            int totalDiceRoll = diceRoll + diceRoll2;
+            rollCounts[totalDiceRoll]++;
+        }
+
+        return rollCounts;
+
+    }
+}
+
+// CLASS 2
+// For each # possible to be rolled
+// print the number
+// Calculate the percentage of times it was rolled
+// print * for each percent it was rolled
+//( method to sub character for number)
+
+
+
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to the Dice Roll Simulation!");
+        // Ask user how many times to roll the dice
+        System.Console.WriteLine("Please enter how many times you want to roll the dice");
+        
+        // Get the number of rolls from the user
+        if (!int.TryParse(Console.ReadLine(), out int numrolls) || numrolls <= 0)
+        {
+            Console.WriteLine("Invalid input. Please enter a positive integer.");
+            return;
+        }
+        // Create instance of DiceStimulator to link class
+        DiceStimulator diceStimulator = new DiceStimulator();
+
+        // Create dice roll
+        int[] rollResults = diceStimulator.RollDice(numrolls);
+
+        // Print out the histogram results
+        DisplayHistogram(rollResults, numrolls);
+
+        // Print the goodbye message
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+    }
+    private static void DisplayHistogram(int[] rollResults, int totalRolls)
+    {
+        Console.WriteLine("\nResults of Dice Rolling Stimulation:");
+        Console.WriteLine("Each * represents 1% of the total number of rolls.  ");
+        Console.WriteLine("\nThe total number of rolls was " + totalRolls);
+        for (int i = 2; i <= 12; i++)
+        {
+            int count = rollResults[i];
+            double percentage = ((double)count / totalRolls) * 100;
+
+            // Generate histogram where each * represents 1%
+
+            string bar = new string('*', (int)percentage);
+
+            Console.WriteLine($"{i}: {bar} ");
+        }
+    }
+}
